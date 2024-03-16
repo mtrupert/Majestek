ed2bae56333764bddbd63916b32e6c7d53fa3139
CharlotteACarbaugh <113802791+CharlotteACarbaugh@users.noreply.github.com>
2024-03-16 15:45:45 -0500

    POST done (mostly)
    All the POST requests have been done, however, I am unsure how the data will come in from the frontend. I have code that should work for that part but for it may need changing later.

--- backend/index.js
+++ backend/index.js
@@ -27,14 +27,21 @@ app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 });
 
-/*
+
+
 //POST requests
-app.post("/lockers", async (req, res) => {
+
+//POST Locker
+app.post("/lockers/:info", async (req, res) => {
     try {
+        
+        json_info = info
 
+        var command = "INSET INTO Locker (locker_id, room_id, avail_status) VALUES (${info[0]}, ${info[1]}, ${info[2]})"
 
+        db.query(command)
 
-
+        console.log("Information Inserted")
     }
     catch (err) {
         res.status(500).json({
@@ -42,17 +49,83 @@ app.post("/lockers", async (req, res) => {
         });
     }
 });
-*/
+
+
+//POST Equipment
+app.post("//equipment:info", async (req, res) => {
+    try {
+        
+        //Im not sure how to do this part tbh
+        json_info = to_list(info)
+
+        var command = "INSET INTO Equipment (equipment_id, equipment_type_id, avail_status) VALUES (${info[0]}, ${info[1]}, ${info[2]})"
+
+        db.query(command)
+
+        console.log("Information Inserted")
+    }
+    catch (err) {
+        res.status(500).json({
+            message: err,
+        });
+    }
+});
+
+
+//POST Reservation
+app.post("/reservations/:info", async (req, res) => {
+    try {
+        
+        //Im not sure how to do this part tbh
+        json_info = to_list(info)
+
+        var command = "INSET INTO Reservation (reservation_id, equipment_id, locker_id, user_id, reserv_start, reserv_end, reserv_status) VALUES (${info[0]}, ${info[1]}, ${info[2]}, ${info[3]}, ${info[4]}, ${info[5]}, ${info[6]})"
+
+        db.query(command)
+
+        console.log("Information Inserted")
+    }
+    catch (err) {
+        res.status(500).json({
+            message: err,
+        });
+    }
+});
+
+
+
+//POST User
+app.post("/users/:info", async (req, res) => {
+    try {
+        
+        //Im not sure how to do this part tbh
+        json_info = to_list(info)
+
+        var command = "INSET INTO User (user_id, user_name, user_password, user_email, user_role, fal_stu_status) VALUES (${info[0]}, ${info[1]}, ${info[2]}, ${info[3]}, ${info[4]}, ${info[5]})"
+
+        db.query(command)
+
+        console.log("Information Inserted")
+    }
+    catch (err) {
+        res.status(500).json({
+            message: err,
+        });
+    }
+});
+
 
 
 
 
 // select or GET requests
 
-//get reservations
+//GET Reservations
 app.get("/reservations", async (req, res) => {
     try {
-        const result = db.query("SELECT * FROM reservation")
+        const result = db.query("SELECT * FROM Reservation")
+
+        console.log(result)
 
         res.render({ data: result })
     }
@@ -64,11 +137,13 @@ app.get("/reservations", async (req, res) => {
 });
 
 
-//get reservations by user
-app.get("/reservations/:userID", async (req, res) => {
+//GET Reservation by User
+app.get("/reservations/:user_id", async (req, res) => {
     try {
-        UserID = userID
-        const result = db.query("SELECT * FROM reservation where UserID = ${UserID}")
+        UserID = user_id
+        const result = db.query("SELECT * FROM Reservation where user_id = ${UserID}")
+
+        console.log(result)
 
         res.render({ data: result })
     }
@@ -80,10 +155,12 @@ app.get("/reservations/:userID", async (req, res) => {
 });
 
 
-//get lockers
+//GET Lockers
 app.get("/lockers", async (req, res) => {
     try {
-        const result = db.query("SELECT * FROM lockers")
+        const result = db.query("SELECT * FROM Locker")
+
+        console.log(result)
 
         res.render({ data: result })
     }
@@ -95,10 +172,13 @@ app.get("/lockers", async (req, res) => {
 });
 
 
-//get reservations
+//GET Equipment
 app.get("/equipment", async (req, res) => {
     try {
-        const result = db.query("SELECT * FROM equipment")
+        const result = db.query("SELECT * FROM Equipment")
+
+        console.log(result)
+
 
         res.render({ data: result })
     }
@@ -110,10 +190,12 @@ app.get("/equipment", async (req, res) => {
 });
 
 
-//get reservations
+//GET User
 app.get("/users", async (req, res) => {
     try {
-        const result = db.query("SELECT * FROM Users")
+        const result = db.query("SELECT * FROM User")
+
+        console.log(result)
 
         res.render({ data: result })
     }
