216b709fe98ec7a002cd2f9c3742093f416f13e5
mtrupert <michael_rupert1@hotmail.com>
2024-04-02 19:35:53 -0500

    Deleted excess code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,34 +23,9 @@
 
       <!-- Additional Content Sections -->
       <div v-if="currentSection === 'view_inventory'">
-        <!-- Template for view_inventory -->
-        <h2 style="color: black;">View Inventory</h2>
-        <div>
-          <label for="sortSelect">Sort by:</label>
-          <select id="sortSelect" v-model="sortOption">
-            <option value="name">Name</option>
-            <option value="availability">Availability</option>
-          </select>
-        </div>
-        <table class="inventory-table">
-          <thead>
-            <tr>
-              <th @click="sortBy('id')">Student ID</th>
-              <th @click="sortBy('name')">Name</th>
-              <th @click="sortBy('serialNumber')">Serial Number</th>
-              <th @click="sortBy('availability')">Availability</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item, index) in sortedInventory" :key="index">
-              <td>{{ item.id }}</td>
-              <td>{{ item.name }}</td>
-              <td>{{ item.serialNumber }}</td>
-              <td>{{ item.available ? 'Available' : 'Unavailable' }}</td>
-            </tr>
-          </tbody>
-        </table>
-  </div>
+        <h2>View Inventory</h2>
+        <!-- Add content related to viewing inventory-->
+      </div>
 
       <div v-if="currentSection === 'view_tickets'">
         <h2>View Support Tickets</h2>
@@ -83,39 +58,14 @@
 <script>
 export default {
   data() {
-    return {
-      currentSection: 'make_reservation',
-      inventory: [
-        { id: '123', name: 'Laptop', serialNumber: 'ABC123', available: true },
-        { id: '124',name: 'Locker', serialNumber: 'DEF456', available: false },
-        { id: '126',name: 'Locker', serialNumber: 'GHI789', available: true },
-      ],
-      sortOption: 'name'
-    }
-  },
-  computed: {
-    sortedInventory() {
-      return this.inventory.slice().sort((a, b) => {
-        if (this.sortOption === 'name') {
-          return a.name.localeCompare(b.name);
-        } else if (this.sortOption === 'serialNumber') {
-          return a.serialNumber.localeCompare(b.serialNumber);
-        } else if (this.sortOption === 'availability') {
-          return a.available - b.available;
-        }
-      });
-    }
+
   },
   methods: {
     navigate(section) {
       this.currentSection = section;
-    },
-    sortBy(option) {
-      this.sortOption = option;
     }
-  },
+  }
 };
-// Ending Script for inventory
 </script>
 
 <style scoped>
