72d661f0728dc776151ae4ffdc2019fb7a8f91c4
kmleevy26 <98064292+kmleevy26@users.noreply.github.com>
2024-04-04 21:59:20 -0500

    Adjustment to router; Include Pinia Store

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,41 +1,69 @@
 import { createRouter, createWebHistory } from "vue-router"
+import { useLoggedInUserStore } from "@/store/loggedInUsers"
 import makeReserve from '../views/makeReserv.vue'
 import viewReserve from '../views/viewReserv.vue'
 import viewInventory from '../views/viewInvent.vue'
 import viewTickets from '../views/viewTickets.vue'
-import Home from '../views/home.vue'
+import home from '../views/home.vue'
+import login from '../views/login.vue'
+import register from '../views/registration.vue'
 
-const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home
-    },
-    {
-        path: '/makereservation',
-        name: 'make-reservation',
-        component: makeReserve
-    },
-    {
-        path: '/viewreservations',
-        name: 'view-reservations',
-        component: viewReserve
-    },
-    {
-        path: '/viewinventory',
-        name: 'view-inventory',
-        component: viewInventory
-    },
-    {
-        path: '/viewtickets',
-        name: 'view-tickets',
-        component: viewTickets
-    }
-]
+
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes: [
+        {
+            path: '/makereservation',
+            name: 'make-reservation',
+            component: makeReserve
+        },
+        {
+            path: '/viewreservations',
+            name: 'view-reservations',
+            component: viewReserve
+        },
+        {
+            path: '/viewinventory',
+            name: 'view-inventory',
+            component: viewInventory
+        },
+        {
+            path: '/viewtickets',
+            name: 'view-tickets',
+            component: viewTickets
+        },
+        {
+            path: '/',
+            name: 'home',
+            component: home
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: login
+        },
+        {
+            path: '/registration',
+            name: 'regstration',
+            component: register
+        },
+        
+    ],
+}
+)
+
+router.beforeEach(async (to, from) => {
+    const store = useLoggedInUserStore()
+    if (
+      !store.isLoggedIn &&
+      // ❗️ Avoid an infinite redirect
+      to.name !== 'login'
+    ) {
+      // redirect the user to the login page
+      return { name: 'login' }
+    }
+  })
+
 
 export default router
