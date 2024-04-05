50721fe75cf53eae02fcaaa6aa77c4f96a74adfe
kmleevy26 <98064292+kmleevy26@users.noreply.github.com>
2024-04-04 22:02:07 -0500

    Included Pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,20 @@
 import './assets/main.css'
+import { createPinia } from 'pinia'
 
-import { createApp } from 'vue'
+
+import { createApp, markRaw } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+
+pinia.use(({ store }) => {
+    store.$router = markRaw(router)
+  });
+  
+app.use(pinia)
+app.use(router);
+
+app.mount('#app');
+
