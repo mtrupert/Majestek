69d82a4d9cc4ee0351e4bfd84cb914c9b809edcf
kmleevy26 <98064292+kmleevy26@users.noreply.github.com>
2024-04-06 22:03:30 -0500

    Added admin reservation page

---
+++ frontend/src/views/adminReservations.vue
@@ -0,0 +1,36 @@
+<template>
+
+<div>
+    <b-table striped hover v-for="reservation in reservations"></b-table>
+</div>
+
+</template>
+
+
+<script>
+
+
+import { getAllReservations } from '../services/API';
+
+
+
+export default {
+    mounted() {
+        getAllReservations()
+      .then(response => {
+        // Assign the response data to the reservations data property
+        this.reservations = response;
+      })
+      .catch(error => {
+        console.error('Error fetching reservations:', error);
+      });
+    },
+    data() {
+        return {
+            reservations: []
+        }
+    }
+}
+
+
+</script>
