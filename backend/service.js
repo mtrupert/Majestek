9853f623c956418b3ba7170f88fa7bb5440ec58d
kmleevy26 <98064292+kmleevy26@users.noreply.github.com>
2024-04-07 19:53:57 -0500

    Added service.js

---
+++ backend/service.js
@@ -0,0 +1,52 @@
+const jwt = require('jsonwebtoken');
+var nodeoutlook = require('nodejs-nodemailer-outlook');
+require("dotenv").config();
+var nodemailer = require('nodemailer');
+
+
+// Function to generate One time token to authenticate
+exports.generateToken = (email, user, pass) => {
+    const expirationDate = new Date();
+    expirationDate.setMinutes(expirationDate.getMinutes() + 45);
+    
+    console.log(expirationDate)
+    return jwt.sign({ email, user, pass, expirationDate }, process.env.JWT_SECRET);
+};
+
+// Function to Send Registration Link
+exports.getMailOptions = (email, link) => {
+    let body = 'click link here: ' + link;
+
+    nodeoutlook.sendEmail({
+        auth: {
+            user: "kyleleevy1@outlook.com",
+            pass: "Ciboney1920!"
+        },
+        from: 'kyleleevy1@outlook.com',
+        to: email,
+        subject: 'Testing...',
+        html: body,
+        text: 'this is the text version'
+    });
+
+    return
+}
+
+exports.getMailPassword = (email, link) => {
+    let body = 'click link here to change your password: ' + link;
+
+    nodeoutlook.sendEmail({
+        auth: {
+            user: "kyleleevy1@oulook.com",
+            pass: "Ciboney1920!"
+        },
+        from: "Ciboney1920!",
+        to: email,
+        subject: 'Forgot Password',
+        html: body,
+        text: 'this is the text version'
+    });
+
+    return
+}
+
