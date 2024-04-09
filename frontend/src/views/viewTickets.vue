342d114b1f19ec791255b7c98f35338a226a54be
CISKevinHuynh <kvhuynh4@cougarnet.uh.edu>
2024-04-09 14:15:52 -0500

    inprogress view tickets
    inprogress view tickets

--- frontend/src/views/viewTickets.vue
+++ frontend/src/views/viewTickets.vue
@@ -1,3 +1,28 @@
 <template>
-    
-</template>
+    <div>
+      <b-table striped hover v-for="ticket in tickets"></b-table>
+    </div>
+  </template>
+  
+  <script>
+  import { getAllTickets } from '../services/API';
+  
+  export default {
+    mounted() {
+      getAllTickets()
+        .then(response => {
+          // Assign the response data to the Tickets data property
+          this.tickets = response;
+        })
+        .catch(error => {
+          console.error('Error fetching Tickets:', error);
+        });
+    },
+    data() {
+      return {
+        Tickets: []
+      };
+    }
+  };
+  </script>
+  
