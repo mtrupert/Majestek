69935370c12a99e1ab25cd0906d37e183819e116
kmleevy26 <98064292+kmleevy26@users.noreply.github.com>
2024-04-11 13:53:33 -0500

    Added functions for password reset

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -224,3 +224,28 @@ try {
   throw error;
 }
 }
+
+export async function passwordLink(email, code) {
+  const response = await axiosInstance.post('/forgot-password', {
+    email: email,
+    code: code
+  });
+    console.log(response)
+    return 
+ 
+}
+
+export async function updatePassword(email, password) {
+  const response = await axiosInstance.post('/users/password', {
+    email: email,
+    password, password 
+  });
+  console.log(response);
+  return response
+}
+
+export function generateRandomSixDigitNumber() {
+  // Generate a random number between 100000 (inclusive) and 999999 (inclusive)
+  const randomNumber = Math.floor(Math.random() * 900000) + 100000;
+  return randomNumber;
+}
